ee86f6a948f9abbcabfee5cc0dd4bbe713898ccd
Fabio Masciarelli <fabiomasciarelliwd@gmail.com>
2024-04-16 16:25:31 +0200

    add: bundle02 finished

--- debug-excercises/js-debug/bundle-02/main.js
+++ debug-excercises/js-debug/bundle-02/main.js
@@ -13,52 +13,54 @@
 // ESERCIZIO 1
 function checkAge() {
     const myAge = 32;
-    const message = '';
+    let message;// la variabile deve essere let
 
-    if (myAge < 18) {
+    if (myAge < 18) { 
         message = `Sei troppo giovane! Hai ${myAge} anni!`;
     } else {
         message = 'Hai più di 18 anni!';
     }
+    return message; // bisogna aggiungere il return
 }
-checkAge();
+const result = checkAge(); // bisogna salvare la funzione in una variabile o non vedremo mai il return
+console.log(result); // funzione per capire se si è maggiorenni o no
 
 // ESERCIZIO 2
 function printColorsNumber() {
     const colors = ['blue', 'red', 'yellow', 'green', 'black'];
-    console.log(`Nella mia palette ci sono ${colors.lenght} colori!`);
+    console.log(`Nella mia palette ci sono ${colors.length} colori!`); // era sbagliato .length
 }
-printColorsNumber();
+printColorsNumber(); // funzione che restituisce il numero di colori dentro un array
 
 
 // ESERCIZIO 3
-function addNumbers() {
-    const userNumber = prompt('Inserisci un numero');
-    const total = userNumber + 12;
+// function addNumbers() {
+//     const userNumber = parseInt(prompt('Inserisci un numero')); // bisogna aggiungere il parseInt
+//     const total = userNumber + 12;
 
-    console.log(`Il risultato finale è ${total}`);
-}
-addNumbers();
+//     console.log(`Il risultato finale è ${total}`);
+// }
+// addNumbers(); //funzione che avendo un numero in input restituisce quel valore sommato con 12
 
 
 // ESERCIZIO 4
-function checkAccess() {
-    const addresses = ['mymail@mail.com', 'yourmail@mail.com', 'hermail@mail.com', 'hismail@mail.com'];
-    const userEmail = prompt('Inserisci il tuo indirizzo email');
+// function checkAccess() {
+//     const addresses = ['mymail@mail.com', 'yourmail@mail.com', 'hermail@mail.com', 'hismail@mail.com'];
+//     const userEmail = prompt('Inserisci il tuo indirizzo email');
 
-    let grantAccess = 'false';
+//     let grantAccess = false;// è dichiarato "false" come stringa e non come flag
 
-    if (addresses.includes(userEmail)) {
-        grantAccess = 'true';
-    }
+//     if (addresses.includes(userEmail)) {
+//         grantAccess = true;// è dichiarato "true" come stringa e non come flag
+//     }
 
-    if (grantAccess === true) {
-        console.log('Accesso consentito!');
-    } else {
-        console.log('Accesso negato!');
-    }
-}
-checkAccess();
+//     if (grantAccess === true) {
+//         console.log('Accesso consentito!');
+//     } else {
+//         console.log('Accesso negato!');
+//     }
+// }
+// checkAccess();
 
 
 // ESERCIZIO 5 (suggerimento: c'è un solo errore)
@@ -87,33 +89,6 @@ function checkAccessImproved() {
             console.log('Accesso negato!');
         }
     }
-    checkAccessImproved();
+} // mancava la paresntesi di chiusura
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+checkAccessImproved();
